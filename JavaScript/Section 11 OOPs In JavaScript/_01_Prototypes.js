ace7783a6271136e08d3cd06920a3ffa09870948
Chinmay Shukla <chinmayshukla831@gmail.com>
2026-03-06 00:07:05 +0530

    "added new file"

---
+++ JavaScript/Section 11 OOPs In JavaScript/_01_Prototypes.js
@@ -0,0 +1,13 @@
+let computer = { cpu: 12}
+let hp = {screen: "HD"}
+let JohnHardware = {}
+
+// console.log(computer);
+// console.log(`computer`,computer.__proto__);
+
+// note --> this ( __ ) double underscore is called Dunder prototype used to access the all the protoype but works only in the browser not on the console part.
+
+let myname = "Chinmay     "
+console.log(myname);
+console.log(myname.length);
+console.log(myname.truelength);
