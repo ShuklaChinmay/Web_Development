4d3b85f60bcbab1de4785b116e3494a9866d0c52
Chinmay Shukla <chinmayshukla831@gmail.com>
2026-03-08 14:25:16 +0530

    "updated the files"

--- JavaScript/Section 11 OOPs In JavaScript/_03_OOPs_In_Js.js
+++ JavaScript/Section 11 OOPs In JavaScript/_03_OOPs_In_Js.js
@@ -9,7 +9,7 @@ let car = {
     },
 }
 
-console.log(car.start());
+// console.log(car.start());
 
 // by using constructor we can create as many copies of the object that we want.
 
@@ -27,9 +27,9 @@ Array.prototype.chinmay = function(){
 }
 
 let myArray = [1,2,3];
-console.log(myArray.chinmay())
+// console.log(myArray.chinmay())
 let myNewArray = [1,2,3,4,5];
-console.log(myNewArray.chinmay())
+// console.log(myNewArray.chinmay())
 
 class Vehicle{
     constructor(make,model){
@@ -49,5 +49,93 @@ class Car extends Vehicle{
 
 
 let myCar = new Car("tata" , "harrier")
-console.log(myCar.start());
-console.log(myCar.drive());
+// console.log(myCar.start());
+// console.log(myCar.drive());
+
+
+// Encapsulation
+// Abstraction --> hide the implementations details 
+class BankAccount{
+// by using the '#' it makes the variable will not be able to access outside the scope of the class.
+    #balance = 0;
+
+    deposit(amount){
+        this.#balance += amount;
+        return this.#balance
+    }
+
+    getBalance(){
+        return `$ ${this.#balance}`
+    }
+
+}
+
+let account = new BankAccount();
+// console.log(account.getBalance());
+// console.log(account.deposit(10000));
+// console.log(account.getBalance());
+
+
+// Polymorphism
+
+class Bird{
+
+    fly(){
+        return `bird can fly.......`
+    }
+}
+
+class Pengiuns{
+
+    fly(){
+        return `penguins can't fly.....`
+    }
+}
+
+let bird = new Bird();
+let penguin = new Pengiuns;
+// console.log(bird.fly());
+// console.log(penguin.fly());
+
+// static method are the method which are directly used by the class itself.
+class Calculator{
+    static add(a,b){
+        return (a+b);
+    }
+}
+
+// let add = new Calculator();
+// console.log(add.add(2,3));
+
+// console.log(`-->`,Calculator.add(2,3));
+
+// Getter and Setter
+
+class Employee{
+    #salary;
+    constructor(name , salary){
+        if (salary<0) {
+            throw new Error`invalid number`
+        }
+        this.name = name
+        this.#salary = salary
+    }
+
+    get salary(){
+        return `you are not supposed to see the salary`
+    }
+
+    set salary(value){
+        if (value<0) {
+            console.error("invalid number")           
+        }
+        else{
+            this._salary = value
+        }
+    }
+}
+
+let emp = new Employee("john",-50000);
+// console.log(emp.salary);
+// emp.salary = -60000
+
